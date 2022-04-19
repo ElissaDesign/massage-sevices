5bb11de49fe0b25b7e44167e75c3b7a5aa81f699
Ihimbazwe Elissa <ihimbazwelisa@gmail.com>
2022-04-19 22:56:13 +0200

    Finished header

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976',
+      xl: '1440px'
+    },
+    extend: {
+      colors:{
+        orange: '#F58A52',
+        blue: '#5753FD',
+        gray: '3F3D3D',
+      }
+    },
+  },
+  plugins: [],
+}
